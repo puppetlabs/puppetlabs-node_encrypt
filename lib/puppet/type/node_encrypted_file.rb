9dec03f3e242194443a03bbe6c653cfd2b8214cb
Ben Ford <ben.ford@puppetlabs.com>
2017-10-25 17:57:04 -0700

    Update node_encrypted_file.rb

--- lib/puppet/type/node_encrypted_file.rb
+++ lib/puppet/type/node_encrypted_file.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '../..', 'puppet_x/binford2k/node_encrypt.rb')
+require_relative '../../puppet_x/binford2k/node_encrypt'
 
 Puppet::Type.newtype(:node_encrypted_file) do
   desc "Manage the content of a file by decrypting with the agent certificate"
