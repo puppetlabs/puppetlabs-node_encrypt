96d30f235b1a74031942af93a2b0806ff32c60ea
jordanbreen28 <jordan.breen@perforce.com>
2023-06-05 12:58:18 +0100

    (maint) - exclude redact module in .gitignore

---
+++ spec/fixtures/modules/redact/manifests/init.pp
@@ -0,0 +1,32 @@
+class redact (
+  $param,
+  $redacted,
+  $replaced,
+) {
+  notice("The value of param is ${param}")
+  notice("The value of redacted is ${redacted}")
+  notice("The value of replaced is ${replaced}")
+
+  redact('redacted')
+  redact('replaced', 'a replacement string')
+
+  redact::thing { 'one':
+    param    => 'a param',
+    redacted => 'to be redacted',
+    replaced => 'to be replaced',
+  }
+  redact::thing { 'two':
+    param    => 'a param',
+    redacted => 'to be redacted',
+    replaced => 'to be replaced',
+  }
+  redact::thing {
+    default:
+      param    => 'a param',
+      redacted => 'to be redacted',
+      replaced => 'to be replaced',
+    ;
+    'three':;
+    'four':;
+  }
+}
