96abd9894609e84e31b1fc5be28041d4350fc6ea
jordanbreen28 <jordan.breen@perforce.com>
2023-06-05 13:54:18 +0100

    (CONT-91) - Correct Layout/ArgumentAlignment

--- lib/puppet/parser/functions/node_encrypt.rb
+++ lib/puppet/parser/functions/node_encrypt.rb
@@ -1,13 +1,13 @@
 require_relative '../../../puppet_x/binford2k/node_encrypt'
 
 Puppet::Parser::Functions::newfunction(:node_encrypt,
-  :type  => :rvalue,
-  :arity => 1,
-  :doc   => <<DOC
+                                       :type  => :rvalue,
+                                       :arity => 1,
+                                       :doc   => <<DOC
 This function simply encrypts the String or Sensitive passed to it using the certificate
 belonging to the client the catalog is being compiled for.
 DOC
-) do |args|
+                                      ) do |args|
   content = args.first
   if defined?(Puppet::Pops::Types::PSensitiveType::Sensitive) && content.is_a?(Puppet::Pops::Types::PSensitiveType::Sensitive)
     content = content.unwrap
--- lib/puppet/parser/functions/redact.rb
+++ lib/puppet/parser/functions/redact.rb
@@ -1,5 +1,5 @@
 Puppet::Parser::Functions::newfunction(:redact,
-  :doc => <<DOC
+                                       :doc => <<DOC
 This function will modify the catalog during compilation to remove the named
 parameter from the class from which it was called. For example, if you wrote a
 class named `foo` and called `redact('bar')` from within that class, then the
@@ -27,7 +27,7 @@ This takes an optional second parameter of the value to replace the original
 parameter declaration with. This parameter is required if the class declares
 a type that is not `String` for the parameter you're redacting.
 DOC
-) do |args|
+                                      ) do |args|
   raise Puppet::ParseError, 'The redact function requires 1 or 2 arguments' unless [1,2].include? args.size
 
   param   = args[0]
