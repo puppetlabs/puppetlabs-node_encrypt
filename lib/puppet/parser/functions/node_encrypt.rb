b3c934157321bba22e785875c59dbc4bcf39533b
jordanbreen28 <jordan.breen@perforce.com>
2023-06-05 14:02:46 +0100

    (CONT-91) - Correct Layout/HeredocIndentation

--- lib/puppet/parser/functions/node_encrypt.rb
+++ lib/puppet/parser/functions/node_encrypt.rb
@@ -3,10 +3,10 @@ require_relative '../../../puppet_x/binford2k/node_encrypt'
 Puppet::Parser::Functions::newfunction(:node_encrypt,
                                        :type => :rvalue,
                                        :arity => 1,
-                                       :doc => <<DOC
-This function simply encrypts the String or Sensitive passed to it using the certificate
-belonging to the client the catalog is being compiled for.
-DOC
+                                       :doc => <<~DOC
+                                         This function simply encrypts the String or Sensitive passed to it using the certificate
+                                         belonging to the client the catalog is being compiled for.
+                                       DOC
                                       ) do |args|
   content = args.first
   if defined?(Puppet::Pops::Types::PSensitiveType::Sensitive) && content.is_a?(Puppet::Pops::Types::PSensitiveType::Sensitive)
