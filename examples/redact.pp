11228d962cdcf08e00f4e16462f71644ee9ef3af
jordanbreen28 <jordan.breen@perforce.com>
2023-06-05 10:06:18 +0100

    (maint) - Fix linter errors with example

--- examples/redact.pp
+++ examples/redact.pp
@@ -1,6 +1,26 @@
-class foo ($bar) {
+# Class: foo
+#
+# This class demonstrates the usage of the 'foo' class in Puppet.
+# It takes a parameter 'bar', displays its value, and performs a redaction.
+#
+# Parameters:
+#
+# [*bar*]
+#   The value of the 'bar' parameter.
+#   Data Type: String
+#
+# Usage:
+#
+# class { 'foo':
+#   bar => 'buzzbuzz',
+# }
+#
+# The above code will instantiate the 'foo' class with the 'bar' parameter set to 'buzzbuzz'.
+#
+class foo (String $bar) { # lint:ignore:autoloader_layout 
   notice("The value of bar is ${bar}")
-  redact('bar')
+
+  redact($bar)
 }
 
 class { 'foo':
