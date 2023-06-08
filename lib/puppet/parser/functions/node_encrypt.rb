f27092796ec1e4164f7963ca8eaf540ec562f2bc
jordanbreen28 <jordan.breen@perforce.com>
2023-06-08 13:31:54 +0100

    (maint) - final rubocop fixes

--- lib/puppet/parser/functions/node_encrypt.rb
+++ lib/puppet/parser/functions/node_encrypt.rb
@@ -4,12 +4,11 @@ require_relative '../../../puppet_x/node_encrypt'
 
 Puppet::Parser::Functions.newfunction(:node_encrypt,
                                       type: :rvalue,
-                                      arity: 1,
-                                      doc: <<~DOC
+                                      arity: 1, doc: <<-DOC
                                         This function simply encrypts the String or Sensitive passed to it using the certificate
                                         belonging to the client the catalog is being compiled for.
                                       DOC
-                                     ) do |args|
+) do |args|
   content = args.first
   content = content.unwrap if defined?(Puppet::Pops::Types::PSensitiveType::Sensitive) && content.is_a?(Puppet::Pops::Types::PSensitiveType::Sensitive)
 
